d23577f4c2f6b1465c4a482a47afb23bbfcd2671
Michael Crockett <mike.crockett76@gmail.com>
2019-10-16 21:55:18 +0100

    Drop down menu added

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,10 +1,14 @@
 <template lang="html">
   <div class="">
+    <select class="" name="country-select">
+      <option v-for="(country, index) in countries">{{country.name}}</option>
+    </select>
     <ul>
       <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
     </ul>
   </div>
 </template>
+
 
 <script>
 import listComponent from './listComponent.vue';
