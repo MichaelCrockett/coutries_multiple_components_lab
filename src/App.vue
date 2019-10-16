46eaf6d5becdc5fee0dd15b991510b04d8adb5a5
CallumGrindle <callumgrindle@gmail.com>
2019-10-16 16:17:52 +0100

    api call working

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template lang="html">
-
+  <div class="">
+    <h1>Countries</h1>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => this.countries = data)
+
+  }
+
+
 }
 </script>
 
