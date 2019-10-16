43e0b2e00fd11ab86b8c61c187453d747af421f4
Michael Crockett <mike.crockett76@gmail.com>
2019-10-16 23:00:44 +0100

    "bedtime commit"

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,12 @@ export default {
   },
   mounted(){
     fetch('https://restcountries.eu/rest/v2/all')
-      .then(res => res.json())
-      .then(data => this.countries = data);
+    .then(res => res.json())
+    .then(data => this.countries = data);
 
-      eventBus.$on('country-selected', (country) => {
-        this.selectedCountry = country;
-      });
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    });
 
   },
   components: {
